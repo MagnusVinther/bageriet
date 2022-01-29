8c76aec88680d6bbb76dd87ff75810bc9fcb557b
MagnusVinther <magn790m@elev.techcollege.dk>
2022-01-29 15:07:20 +0100

    Login basic jsx final (no styling)

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 export const Login = () => {
+    const introMessage = "Indtast brugernavn og adgangskode for at logge ind"
+    const [ message, setMessage ] = useState(introMessage);
+    const [ loginData, setLoginData ] = useState('')
+
+    const SubmitLoginForm = async e => {
+        const formData = new FormData(e.target.form)
+        try {
+            const result = await axios.post('https://api.mediehuset.net/token', formData)
+            handleSessionData(result);            
+        }
+        catch(err) {
+            console.error(err)
+            setMessage('Der er sket en fejl. Kunne ikke logge ind')
+        }
+    }
+
+    const handleSessionData = resp => {
+        if(resp.status === 201) {
+            sessionStorage.setItem('token', JSON.stringify(resp.data))
+            setLoginData(resp.data)
+            setMessage(`Du er logget ind som ${resp.data.username}`)
+        }
+    }
+
+    const logOUt = () => {
+        sessionStorage.removeItem('token')
+        setLoginData('')
+        setMessage(introMessage)
+    }
 
     return (
-        <h2>Hej her er loginpage</h2>
+        <div>
+            <h1>Login</h1>
+            <p>{message}</p>
+            {!loginData ?
+            <form method="post">
+                <div>
+                    <label htmlFor="username">Brugernavn: </label>
+                    <input type="text" name="username" id="username" />
+                </div>
+                <div>
+                    <label htmlFor="password">Adgangskode: </label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div>
+                    <button type="button" onClick={SubmitLoginForm}>Login</button>
+                    <button type="reset">Nulstil</button>
+                </div>
+            </form>
+            :
+            <form>
+                <button type="button" onClick={logOUt}>Log out</button>
+            </form>
+            }
+        </div>
     )
 }
